1936a51f027168220b8fd80d3bd94dd9d62bc8a0
dungvn3000@gmail.com <ducdung2512>
2013-04-04 14:41:40 +0700

    Add Topic Tab.

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+    'server_url' : 'http://vketnoi.com:8088/'
+};
--- popup.js
+++ popup.js
@@ -1,5 +1,30 @@
 $(function ($) {
+    $.getJSON(config.server_url + 'user/topic', function (toipcs) {
+            $.each(toipcs, function (i, topic) {
+                var data = {
+                    topic: topic,
+                    active: function() {
+                        if(i == 0) {
+                            return 'active';
+                        } else {
+                            return '';
+                        }
+                    }
+                };
+                var navTopicItem = ich.navTopicItem(data);
+                $('.topic-nav').append(navTopicItem);
 
+                var topicItem = ich.topicItem(data);
+                $('.topic-content').append(topicItem);
+            });
 
+            $('#login-message').hide();
+            $('.topic-container').show();
 
+            $('.topic-nav a').click(function(e) {
+                e.preventDefault();
+                $(this).tab('show');
+            });
+        }
+    );
 });
