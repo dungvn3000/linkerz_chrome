891bdbd7da1c84dc466dba764584bfe54127fa8c
dungvn3000@gmail.com <ducdung2512>
2013-04-12 18:46:28 +0700

    Revert last commit.

--- menu.js
+++ menu.js
@@ -1,22 +1,41 @@
 var contexts = ["page"];
 
 var parent = chrome.contextMenus.create({
-    'title': 'Quan tam',
-    'onclick': function (info, tab) {
-        onMenuItemClick(info, tab)
-    }
+    'title': 'Quan tam'
 });
 
-function onMenuItemClick(info, tab) {
+$.getJSON(config.server_url + '/user/topic', function (toipcs) {
+    $.each(toipcs, function (i, topic) {
+        chrome.contextMenus.create({
+            'title': topic.name,
+            'parentId': parent,
+            'onclick': function (info, tab) {
+                onMenuItemClick(info, tab, topic)
+            }
+        });
+    });
+
+    //create and post into a new topic
+    chrome.contextMenus.create({
+        'title': 'New Group',
+        'parentId': parent,
+        'onclick': function (info, tab) {
+            var answer = prompt("Topic name?");
+        }
+    });
+});
+
+
+function onMenuItemClick(info, tab, topic) {
     $.ajax({
         'url': config.server_url + "/post/link",
         'method': 'post',
         'data': {
-            'url': info.pageUrl
+            'url': info.pageUrl,
+            'topicId': topic.id
         },
         'success': function (data) {
-            alert("Quan tam thanh Cong!")
+            alert("Them thanh Cong!")
         }
     });
 }
-
